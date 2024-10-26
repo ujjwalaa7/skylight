b38bf71c95a28c075e2c666e2b4b1e169752cb97
aparnaroy <aparnar@udel.edu>
2024-10-26 16:18:07 -0400

    added baseline code

---
+++ src/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function About() {
+  return (
+    <>
+      <div className="About">
+      </div>
+    </>
+  );
+}
+
+export default About;
